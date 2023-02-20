fb45ab3afd4f7a3bfa702e99894ff150c2916fea
vladimir-mamaev <v06455089@gmail.com>
2023-02-20 15:40:17 +0200

    Initial commit

---
+++ src/test/java/GridTest.java
@@ -0,0 +1,22 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class GridTest {
+    @Test
+    public void testF() throws MalformedURLException, InterruptedException {
+        System.setProperty("webdriver.gecko.driver", "src/test/resources/geckodriver.exe");
+        FirefoxOptions capabilities = new FirefoxOptions();
+        capabilities.setBinary("C:\\Program Files\\Mozilla Firefox\\firefox.exe");
+        WebDriver driver = new RemoteWebDriver(new URL("http://192.168.1.140:4444/wd/hub"), capabilities);
+        driver.get("https://uk-ua.facebook.com/");
+        Assertions.assertTrue(driver.getTitle().contains("Facebook"));
+        Thread.sleep(5000);
+        driver.quit();
+    }
+}
---
+++ src/test/java/OpenPageFirefox.java
@@ -0,0 +1,55 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.parallel.Execution;
+import org.junit.jupiter.api.parallel.ExecutionMode;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+
+import static org.junit.Assert.assertEquals;
+
+@Execution(ExecutionMode.CONCURRENT)
+public class OpenPageFirefox {
+    WebDriver driver;
+
+    @BeforeEach
+    public void before() {
+        String browser = System.getProperty("browser");
+        if (browser.equals("chrome")) {
+            System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+            driver = new ChromeDriver();
+        }
+        if (browser.equals("gecko")) {
+            System.setProperty("webdriver.gecko.driver", "src/test/resources/geckodriver.exe");
+            FirefoxOptions capabilities = new FirefoxOptions();
+            capabilities.setBinary("C:\\Program Files\\Mozilla Firefox\\firefox.exe");
+            driver = new FirefoxDriver(capabilities);
+        }
+    }
+
+    @AfterEach
+    public void after() {
+        driver.quit();
+    }
+
+    @Test
+    public void openGoogle() {
+        driver.get("https://www.google.com/");
+        assertEquals("Google", driver.getTitle());
+    }
+
+    @Test
+    public void openYoutube() {
+        driver.get("https://www.youtube.com/");
+        assertEquals("YouTube", driver.getTitle());
+    }
+
+    @Test
+    public void openVoutube() {
+        driver.get("https://www.facebook.com/");
+        assertEquals("Facebook - log in or sign up", driver.getTitle());
+    }
+}
+
